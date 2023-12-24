64966ac3eab0ef5d9f0d6987ed2c4992fc29b57b
Kirill <forworkskirill@gmail.com>
2023-12-25 03:16:58 +0500

    wip try fix migration

---
+++ src/migrations/20231224220929-add_column_viewers_to-Gifs.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const TABLE_NAME = "Gifs";
+const COLUMN_NAME = "viewers";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn(TABLE_NAME, COLUMN_NAME, {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeColumn(TABLE_NAME, COLUMN_NAME);
+  },
+};
