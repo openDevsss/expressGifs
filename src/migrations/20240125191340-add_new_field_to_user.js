126cb3e3e3d329700b80ada1fdb263baf613b312
Kirill <forworkskirill@gmail.com>
2024-01-26 00:31:23 +0500

    feat: create new column

---
+++ src/migrations/20240125191340-add_new_field_to_user.js
@@ -0,0 +1,32 @@
+const table = "Users";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn(table, "phone", {
+      type: Sequelize.INTEGER,
+      allowNull: true,
+    });
+
+    await queryInterface.addColumn(table, "fullName", {
+      type: Sequelize.STRING,
+      allowNull: true,
+    });
+
+    await queryInterface.addColumn(table, "lastName", {
+      type: Sequelize.STRING,
+      allowNull: true,
+    });
+
+    await queryInterface.addColumn(table, "location", {
+      type: Sequelize.STRING,
+      allowNull: true,
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.removeColumn(table, "phone");
+    await queryInterface.removeColumn(table, "fullName");
+    await queryInterface.removeColumn(table, "lastName");
+    await queryInterface.removeColumn(table, "location");
+  },
+};
